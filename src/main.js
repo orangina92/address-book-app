aff6d5bbd72fd8745bc16c9cff94c96be3f27c70
orangina92 <user@USER-no-MacBook-Pro.local>
2019-05-05 23:00:54 +0900

    firebaseの導入

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const Config = {
+  apiKey: "AIzaSyBbum-KA1mvnB7s0zbrviePpJzNg666bfE",
+  authDomain: "adress-book-app.firebaseapp.com",
+  databaseURL: "https://adress-book-app.firebaseio.com",
+  projectId: "adress-book-app",
+  storageBucket: "adress-book-app.appspot.com",
+  messagingSenderId: "582892057513",
+  appId: "1:582892057513:web:23d6f981adf4543c"
+};
+firebase.initializeApp(Config);
 
 new Vue({
   router,
